4144273f72c7228ed80031cf5571a1d1d6e7466d
Jean-Michel Tremblay <jeanmi.tremblay@gmail.com>
2024-11-29 09:00:11 -0500

    scripts to deploy
    deal with execStop
    
    make sure you stop and rm old container at boot time

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker run\
-    -v /home/jm/sites/jmopines/public:/usr/share/nginx/html\
-    -p 10555:80 nginx:alpine 
+docker run \
+  --name jmopines-nginx \
+  -v /home/jm/sites/jmopines/public:/usr/share/nginx/html \
+  -v /home/jm/sites/jmopines/nginx.conf:/etc/nginx/nginx.conf \
+  -p 10555:80 \
+  --entrypoint="nginx" \
+  nginx:alpine -g "daemon off;"
