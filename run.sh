5bb0057822e27f483c533b97e483ff714cb99b7a
Jean-Michel Tremblay <jeanmi.tremblay@gmail.com>
2024-11-29 10:11:27 -0500

    run script to stop rm container before booting

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Check if the container is running and stop it if it is
+if docker ps -q -f name=jmopines-nginx; then
+    docker stop jmopines-nginx
+fi
+
+# Check if the container exists and remove it if it does
+if docker container inspect jmopines-nginx > /dev/null 2>&1; then
+    docker rm jmopines-nginx
+fi
+
 docker run \
   --name jmopines-nginx \
   -v /home/jm/sites/jmopines/public:/usr/share/nginx/html \
